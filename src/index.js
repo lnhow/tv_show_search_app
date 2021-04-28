f902f9526c07cd73369b0735b11c47004b54b7ab
Lê Nguyên Hào <18120355@student.hcmus.edu.vn>
2021-04-28 09:26:00 +0700

    Notes on what each react imports are & title index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';        //React things
+import ReactDOM from 'react-dom'; //React to HTML DOM interactions
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //DOM container to render this component
 );
 
 // If you want to start measuring performance in your app, pass a function
