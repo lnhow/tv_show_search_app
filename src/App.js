6af8fa01f8dff50349beaabe7f4676e575fb5eec
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-01 15:51:27 +0700

    Refactor "App" component from function to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';  <-- Webpack allow svg imports
+import React from 'react';
 import './App.css';
 import AppIntro from "./components/intro";  //Directory
 
@@ -6,29 +7,30 @@ import AppIntro from "./components/intro";  //Directory
 //   return 5;
 // }
 
-function App() {
-  return (
-    //This comment will not show up in user's browser but can't be written inside a JSX component
-    /*This comment will behave the same as the one above*/
-    <div className="App">
-      {/* This comment is a comment inside a JSX component and can't be written like the 2 above*/}
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1 className="App-title">TV series App</h1>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      //This comment will not show up in user's browser but can't be written inside a JSX component
+      /*This comment will behave the same as the one above*/
+      <div className="App">
+        {/* This comment is a comment inside a JSX component and can't be written like the 2 above*/}
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">TV series App</h1>
+        </header>
 
-      {/*
-        Special feature to call a functional component with the same name, 
-        function name have to be capitalize the first letter to differentiate from normal HTML tags
-        <AppIntro />  //The normal way
-        {AppIntro()}  //This is OK as it is just JS inside a "{" and "}"
-        <Test/>       //It is also ok for function that return a non-JSX,
-                      //returns will be convert to string & show on the browser
-        */}
-        <AppIntro message="An app to display your favorite TV shows"/>
-
-    </div>
-  );
+        {/*
+          Special feature to call a functional component with the same name, 
+          function name have to be capitalize the first letter to differentiate from normal HTML tags
+          <AppIntro />  //The normal way
+          {AppIntro()}  //This is OK as it is just JS inside a "{" and "}"
+          <Test/>       //It is also ok for function that return a non-JSX,
+                        //returns will be convert to string & show on the browser
+          */}
+          <AppIntro message="An app to display your favorite TV shows"/>
+      </div>
+    );
+  }
 }
 
 export default App;
