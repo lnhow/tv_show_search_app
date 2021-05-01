706139199145727c939d10b6fa9d84e611f9933c
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-01 17:38:08 +0700

    Add search series function

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import SeriesList from '../../components/SeriesList';
 
+//An API for TV shows infomation
+const queryFormat = "http://api.tvmaze.com/search/shows?q=";
+
 // Container component is concern with making things works
 // For Presentation component to display the looks of it
 class Series extends React.Component {
   render() {
     return (
       <div>
-        <p>Current TV shows: {this.state.series.length}</p>
+        <div>
+          <input type="text" onChange={this.onSearchInputChange}/>
+        </div>
+        <p>Current TV show results: {this.state.series.length}</p>
         <SeriesList list={this.state.series} />
       </div>
     )
@@ -23,14 +29,17 @@ class Series extends React.Component {
   // Call after the first render() is called when component first mount
   // Trigger re-rendering if setting state
   componentDidMount() {
-    //An API for TV infomation
-    fetch("http://api.tvmaze.com/search/shows?q=Vikings")
-    .then(response => response.json())  //.json return a Promise
+  }
+
+  onSearchInputChange = (event) => {
+    this.fetchData(event.target.value)
     .then(json => this.setState({ series: json }));
+  }
 
-    // const series = ["Show 1", "Show 2"];
-
-    // this.setState({ series });
+  fetchData(searchString) {
+    const query  = queryFormat + searchString;
+    return fetch(query)
+    .then(response => response.json())  //.json return a Promise
   }
 }
 
