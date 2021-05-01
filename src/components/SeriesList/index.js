532f593493fe653479a88138c99624745ee43709
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-01 17:13:54 +0700

    Refactor list style & add keys to list items

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,11 +1,15 @@
 //import React from 'react';
+import "./index.css"; // Tell webpack that series list use this style
 
 const SeriesList = (props) => {
   return (
     <div>
-      <ul>
+      <ul className="Series-list">
         {props.list.map(series => 
-          <li>{series.show.name}</li>
+          <li key={series.show.id}>
+            {/* React list have to get have a unique key */}
+            {series.show.name}
+          </li>
         )}
       </ul>
     </div>
