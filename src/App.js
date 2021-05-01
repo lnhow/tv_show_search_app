507c14ef1addfa650ddb4605c21219606bd894ff
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-01 15:23:01 +0700

    Comments on comment & component calling inside JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 // import logo from './logo.svg';  <-- Webpack allow svg imports
 import './App.css';
 
+
+function AppIntro(props) {
+  return <p className="App-intro">
+    This is from a functional component
+  </p>
+}
+
+// function Test() {
+//   return 5;
+// }
+
 function App() {
   return (
+    //This comment will not show up in user's browser but can't be written inside a JSX component
+    /*This comment will behave the same as the one above*/
     <div className="App">
+      {/* This comment is a comment inside a JSX component and can't be written like the 2 above*/}
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="App-title">TV series App</h1>
       </header>
+      
+      <AppIntro />  {/*Special feature to call a functional component*/}
+      {/*AppIntro() /*This is OK as it is just JS inside a "{" and "}" */}  
+      {/*<Test/>   /*This is also ok*/}
     </div>
   );
 }
